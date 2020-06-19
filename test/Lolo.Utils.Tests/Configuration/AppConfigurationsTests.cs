8ab2df4fe1244e663ff4099e1bb9e2c569d90a9d
heweigeng1 <6318275@qq.com>
2020-06-20 00:10:55 +0800

    update

---
+++ test/Lolo.Utils.Tests/Configuration/AppConfigurationsTests.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+using Lolo.Utils.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace Lolo.Utils.Configuration.Tests
+{
+    public class AppConfigurationsTests
+    {
+        public AppConfigurationsTests()
+        {
+        }
+
+        [Fact()]
+        public void GetTest()
+        {
+            //Arrange
+            string path= AppContext.BaseDirectory.Split("bin")[0];
+
+            //ACT
+            var config = AppConfigurations.Get(path);
+
+            //Assert 
+            Assert.True(config["CashierVersion:Version"]== "0.9.0", "This test needs an implementation");
+        }
+    }
+}
